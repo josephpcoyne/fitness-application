f4b54b249105a13506ea7373a067219f7ecaf5dd
josephpcoyne <josephpcoyne@gmail.com>
2018-10-21 02:30:38 -0400

    adds index trainer profile cards

--- src/App.vue
+++ src/App.vue
@@ -23,19 +23,18 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
-#nav {
+/*#nav {
   padding: 30px;
 }
-
-#nav a {
+*/
+/*#nav a {
   font-weight: bold;
   color: #2c3e50;
-}
+}*/
 
-#nav a.router-link-exact-active {
+/*#nav a.router-link-exact-active {
   color: #42b983;
-}
+}*/
 </style>
--- src/views/Trainers.vue
+++ src/views/Trainers.vue
@@ -1,39 +1,26 @@
-Vue.component('star-rating', StarRating);
+
 <template>
   <div class="home">
-    <div class="uk-grid-small uk-child-width-1-2@s" uk-grid >
+    <div class="uk-grid-small uk-child-width-1-2@s" uk-grid>
 <!-- Trainer Profile Cards -->
-			<div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid v-for="trainer in trainers">
+			<div class="uk-card uk-card-hover uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid v-for="trainer in trainers">
 			    <div class="uk-card-media-left uk-cover-container">
-			        <img src="../assets/trainer_profile3.jpg" uk-cover />
-			        <canvas width="600" height="400"></canvas>
+			      <img :src="getImgUrl(trainer.profile_picture_url)" uk-cover>
+			      <canvas width="600" height="400"></canvas>
 			    </div>
 			    <div>
-			        <div class="uk-card-body">
-			            <h3 class="uk-card-title">{{ trainer.first_name }} {{ trainer.last_name }}</h3>
-			            <p>{{trainer.bio}}</p>
-
-									<div class="uk-child-width-1-4 uk-grid-small uk-text-center" uk-grid v-for="tag in trainer.tags">
-									    <div>
-									        <div class="uk-tile uk-tile-primary uk-padding-remove" >
-									            <p  class="uk-h5">{{tag.name}}</p>
-									        </div>
-									    </div>
-									</div>
-
-
-
-
-
-
-			        </div>
+			    	<div class="uk-card-body">
+			      	<h3 class="uk-card-title">{{ trainer.first_name }} {{ trainer.last_name }}</h3>
+			        <p>{{trainer.bio}}</p>
+							<!-- trainer tags -->
+			        <template v-for="tag in trainer.tags">
+			   				<span class="uk-label uk-margin-right uk-card-footer">{{ tag.name }} </span>
+							</template>
+							<!-- trainer tags end -->
+			     	</div>
 			    </div>
 			</div>
 <!-- Trainer Profile Cards End -->
-<!--     	<h4>{{trainer.first_name}} {{trainer.last_name}}</h4>
-    	{{trainer.rating}} stars
-    	<p>{{trainer.bio}}</p>
-		<li>{{trainer.tags}}</li> -->
     </div>
   </div>
 </template>
@@ -42,7 +29,6 @@ Vue.component('star-rating', StarRating);
 </style>
 
 <script>
-var StarRating = require('vue-star-rating');
 var axios = require("axios");
 export default {
 	data: function() {
@@ -56,7 +42,12 @@ export default {
 			this.trainers = response.data;
 		});
 	},
-	methods: {},
+	methods: {
+		getImgUrl(filename) {
+    var images = require.context('../assets/', false, /\.jpg$/)
+    return images('./' + filename + ".jpg")
+  }
+	},
 	computed: {}
 };
 </script>
